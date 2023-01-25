ebc47ede41f74e2d3328f4a577a1a24ac5f0a63d
Wolney Carneiro <wolneycarneiro91@gmail.com>
2023-01-24 23:06:20 -0300

    Corrigindo autenticação

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dbUser = process.env.DB_USERNAME;
+const dbPassword= process.env.DB_PASSWORD;
+
 async function main(){
     try{
         mongoose.set('strictQuery',true);
-        await mongoose.connect('mongodb+srv://cesarcarneiro:q1w2e3r4@cluster0.q4wc6zx.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.q4wc6zx.mongodb.net/?retryWrites=true&w=majority`)        
         console.log('conectado');
     }catch(error){
         console.log(error);
